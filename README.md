798bd43986a906456abd5dff447e7381ffc22280
artimonist <artimonist@gmail.com>
2025-01-15 16:54:16 +0800

    ver 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # artimonist-cli
 A tool for generating mnemonics based on diagrams.
+
+**`> artimonist-cli --help`**
+
+```
+Usage: artimonist-cli [OPTIONS] <CONTENT> [INDICES]...
+
+Arguments:
+  <CONTENT>     diagram chars
+  [INDICES]...  diagram indices: (row, col), 0 <= row < 7, 0 <= col < 7
+
+Options:
+  -t, --target <TARGET>  generate target [default: mnemonic] [possible values: mnemonic, wif, xpriv, pwd]
+  -c, --count <COUNT>    generate count [default: 1]
+  -s, --salt <SALT>      salt
+  -h, --help             Print help
+```
+
+### mnemonic
+**`> artimonist-cli 【1$≈⅞£】 (0,0) (1,1) (2,2) (3,3) (4,4) (5,5) (6,6)`**
+```
+0: melody stove spell vessel gold cricket public ensure family seek piano mechanic trap truck roast bind alley donor runway miracle entry crop act vocal
+
+```
+
+### wif
+**`> artimonist-cli --target wif --count 5 【1$≈⅞£】 (0,0) (1,1) (2,2) (3,3) (4,4) (5,5) (6,6)`**
+```
+0: ( 38FPFdaGWejCRawvsCbSiuj8AgAJGJ1vNz, KzWLnHF5ESw43o6Uy4ZCixVT8LEA4cQnF3m47FQp6WwBn3QzmbsZ )
+1: ( 3QRD7qGyF2QAXrRiRuCr6H2D4DEXLRicco, KxpzGv35MPYLFammP7hqGgGCc81g6Gx5cYfLRapPURmSS9oEPJxT )
+2: ( 32EUNg5vHukrVkE2PA2hMawfeoeXeoJXGj, L4evBDU2brn915UXnoiisDPjET8xkCuwTo4Ef5C6ki3RPT3vrWzM )
+3: ( 35vpqRzaJ83G2DpRBSrWaaN3ELWfNB3BCZ, L1uXQySss2vSkJf6BvqLwjFemwqgKawtKToW922paWrcrV95Vs6P )
+4: ( 328Z2MBojc2f4bFo9RvxLJQizP4SbcWLic, L4YQmGr4582nGVirJBd5ktu1KnAN12VzBfx2V22HQgWNUXEDvctJ )
+``` 
+
+### xpriv
+**`> artimonist-cli --target xpriv --salt artimonist 【1$≈⅞£】 (0,0) (1,1) (2,2) (3,3) (4,4) (5,5) (6,6)`**
+```
+0: xprv9s21ZrQH143K26RCtmAMZxtw7UJ9EzVwVtftmszx9y1LC4PktDeFNAXFT7Qwdpy1vQHkV5fiRxUbX8NznqvkKJQaU5Nj74hzERT8mdcmPw5
+```
+### pwd
+**`> artimonist-cli -t pwd -c 10 【1$≈⅞£】 (0,0) (1,1) (2,2) (3,3) (4,4) (5,5) (6,6)`**
+```
+0: FtbFj#LWmrsK@n$UhpK1
+1: tkyS%EG5nf%h1Y5U*g75
+2: YTv1oFxX4#Nys75LqDhb
+3: 3Kq37Cso%1Fcn$8$D7EJ
+4: r%nJ4817&Q8L9N*AbbLk
+5: wNTDNhzZ&NuM5@9bEP6F
+6: &PUcYESkHQ%TimRTJW&8
+7: %61pCvURZgqTAePA7HDi
+8: Yu%k4b2VjL8GMBFHknvS
+9: H6K&BeU34b2ycsZK2cbe
+```
---
+++ src/main.rs
@@ -0,0 +1,97 @@
+use artimonist::{Diagram, Error, SimpleDiagram, Xpriv, BIP85};
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser)]
+struct Cli {
+    /// generate target
+    #[arg(short, long, default_value = "mnemonic")]
+    target: Target,
+    /// generate count
+    #[arg(short, long, default_value_t = 1, value_parser = clap::value_parser!(u8).range(..20))]
+    count: u8,
+    /// salt
+    #[arg(short, long)]
+    salt: Option<String>,
+    /// diagram chars
+    content: String,
+    /// diagram indices: (row, col), 0 <= row < 7, 0 <= col < 7
+    #[arg(value_parser = parse_indices)]
+    indices: Vec<(u8, u8)>,
+}
+
+#[derive(ValueEnum, Clone, Copy, Default, Debug)]
+enum Target {
+    #[default]
+    Mnemonic,
+    Wif,
+    Xpriv,
+    Pwd,
+}
+
+fn parse_indices(s: &str) -> Result<(u8, u8), String> {
+    if let Some((row, col)) = s
+        .trim_matches(&['(', ')'])
+        .split_once(',')
+        .map(|(a, b)| (a.as_bytes()[0] - b'0', b.as_bytes()[0] - b'0'))
+        .filter(|&(a, b)| a < 7 && b < 7)
+    {
+        return Ok((row, col));
+    }
+    Err("0 <= row < 7 && 0 <= col < 7".to_owned())
+}
+
+fn main() -> Result<(), Error> {
+    let args = Cli::parse();
+
+    let chars = args.content.chars().map(|v| Some(v)).collect();
+    let indices: Vec<(usize, usize)> = args
+        .indices
+        .into_iter()
+        .map(|(a, b)| (a as usize, b as usize))
+        .collect();
+    let salt = args.salt.unwrap_or_default();
+
+    let diagram = SimpleDiagram::from_items(chars, &indices)?;
+    let master = diagram.to_master(salt.as_bytes())?;
+    let results: Vec<String> = (0..args.count as u32)
+        .map(|i| generate(&master, args.target, i).unwrap_or_default())
+        .collect();
+
+    println!("");
+    results.into_iter().enumerate().for_each(|(i, v)| {
+        println!("{i}: {v}");
+    });
+
+    Ok(())
+}
+
+fn generate(master: &Xpriv, t: Target, i: u32) -> Option<String> {
+    use artimonist::bitcoin::{
+        secp256k1::Secp256k1, Address, CompressedPublicKey, NetworkKind, PrivateKey,
+    };
+    macro_rules! p2shwpkh {
+        ($x: expr) => {
+            Address::p2shwpkh(
+                &CompressedPublicKey::from_private_key(
+                    &Secp256k1::default(),
+                    &PrivateKey::from_wif(&$x).unwrap(),
+                )
+                .unwrap(),
+                NetworkKind::Main,
+            )
+        };
+    }
+
+    let bip85_wif = |i| match master.bip85_wif(i) {
+        Ok(wif) => format!("( {}, {wif} )", p2shwpkh!(wif)),
+        _ => "".to_owned(),
+    };
+
+    match t {
+        Target::Mnemonic => master.bip85_mnemonic(Default::default(), 24, i),
+        Target::Xpriv => master.bip85_xpriv(i),
+        Target::Wif => Ok(bip85_wif(i)),
+        Target::Pwd => master.bip85_pwd(Default::default(), 20, i),
+    }
+    .map_or(None, |v| Some(v))
+}
