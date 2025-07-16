d1601b247bcaf9a36f5e8c5de34402120471cd75
artimonist <artimonist@gmail.com>
2025-07-16 09:43:36 +0000

    simplify unicode parse

--- src/diagram/matrix.rs
+++ src/diagram/matrix.rs
@@ -11,13 +11,11 @@ where
     fn from_file(path: &str) -> anyhow::Result<Self>;
     /// load 7 * 7 matrix from inquire
     fn from_inquire() -> anyhow::Result<Self>;
-    /// parse a line of input into a vector of cell values
-    fn parse_values(line: &str) -> Vec<Option<T>>;
 }
 
 impl<T> LoadMatrix<T> for Matrix<T, 7, 7>
 where
-    T: Transformer + std::fmt::Debug,
+    T: UniParser + std::fmt::Debug,
 {
     fn from_file(path: &str) -> anyhow::Result<Self> {
         let mvs = BufReader::new(File::open(path)?)
@@ -25,7 +23,7 @@ where
             .take(7)
             .map(|ln| {
                 if let Ok(str) = ln {
-                    Self::parse_values(&str)
+                    UniParser::parse_values(&str)
                 } else {
                     eprintln!("Error reading line: {}", ln.unwrap_err());
                     vec![]
@@ -43,43 +41,41 @@ where
                 .with_help_message("Fill characters in quotes.")
                 .prompt();
             if let Ok(ln) = ln {
-                mvs.push(Self::parse_values(&ln));
+                mvs.push(UniParser::parse_values(&ln));
             } else {
                 eprintln!("Error reading input for row {i}");
             }
         });
         Ok(mvs.to_matrix())
     }
+}
 
-    fn parse_values(line: &str) -> Vec<Option<T>> {
+trait UniParser
+where
+    Self: Sized,
+{
+    fn decode(s: &str) -> Option<Self>;
+
+    fn parse_values(line: &str) -> Vec<Option<Self>> {
         line.strip_prefix('"')
             .unwrap_or(line)
             .strip_suffix('"')
             .unwrap_or(line)
             .split(r#""  ""#)
             .take(7)
-            .map(Transformer::decode)
+            .map(Self::decode)
             .collect::<Vec<_>>()
     }
 }
 
-pub trait Transformer
-where
-    Self: Sized,
-{
-    fn decode(v: &str) -> Option<Self>;
-}
-
-impl Transformer for char {
-    #[inline(always)]
-    fn decode(v: &str) -> Option<Self> {
-        unicode_decode(v).chars().next()
+impl UniParser for char {
+    fn decode(s: &str) -> Option<char> {
+        unicode_decode(s).chars().next()
     }
 }
 
-impl Transformer for String {
-    #[inline(always)]
-    fn decode(v: &str) -> Option<Self> {
-        Some(unicode_decode(v).chars().take(20).collect())
+impl UniParser for String {
+    fn decode(s: &str) -> Option<String> {
+        Some(unicode_decode(s).chars().take(20).collect())
     }
 }
