127f2b76f4802aca3b71709099a46e8a235a1f03
artimonist <artimonist@gmail.com>
2025-08-20 03:19:10 +0000

    refactor matrix load

--- src/diagram/execute.rs
+++ src/diagram/execute.rs
@@ -1,13 +1,19 @@
-use super::{DiagramCommand, matrix::LoadMatrix, output::ConsoleOutput};
-use crate::utils::{inquire_password, select_language};
-use artimonist::{ComplexDiagram, Language, Matrix, SimpleDiagram};
+use std::any::type_name;
 
-impl crate::Execute for DiagramCommand<SimpleDiagram> {
-    fn execute(&mut self) -> anyhow::Result<()> {
+use super::DiagramCommand;
+use crate::utils::{inquire_password, select_language, unicode_decode};
+use artimonist::{Diagram, GenericDiagram, Language};
+
+type Result<T> = anyhow::Result<T>;
+
+const WORD_MAX_LENGTH: usize = 20;
+
+impl<T: GenericDiagram> crate::Execute for DiagramCommand<T> {
+    fn execute(&mut self) -> Result<()> {
         // load matrix data from file or inquire it from user
-        let mx = match &self.file {
-            Some(file) => Matrix::<char>::from_file(file)?,
-            None => Matrix::<char>::from_inquire()?,
+        let items = match &self.file {
+            Some(file) => from_art_file(file)?,
+            None => from_inquire()?,
         };
 
         // choose a mnemonic language if needed
@@ -16,36 +22,68 @@ impl crate::Execute for DiagramCommand<SimpleDiagram> {
         }
 
         // inquire the encryption password as salt
-        if self.password.is_none() {
-            self.password = Some(inquire_password(true)?);
-        }
+        let password = match &self.password {
+            Some(v) => v.to_string(),
+            None => inquire_password(true)?,
+        };
 
         // output the diagram's result
-        SimpleDiagram(mx).display(self)?;
+        let vs = items.iter().map(|s| s.as_ref()).collect::<Vec<_>>();
+        let master = if type_name::<T>().contains("SimpleDiagram") {
+            vs.art_simple_master(&password)
+        } else if type_name::<T>().contains("ComplexDiagram") {
+            vs.art_complex_master(&password)
+        } else {
+            return Err(anyhow::anyhow!("Unsupported diagram type"));
+        };
         Ok(())
     }
 }
 
-impl crate::Execute for DiagramCommand<ComplexDiagram> {
-    fn execute(&mut self) -> anyhow::Result<()> {
-        // load the matrix from file or inquire it from user
-        let mx = match &self.file {
-            Some(file) => Matrix::<String>::from_file(file)?,
-            None => Matrix::<String>::from_inquire()?,
-        };
+fn from_art_file(path: &str) -> Result<Vec<String>> {
+    use std::io::{BufRead, BufReader};
+    let mvs = BufReader::new(std::fs::File::open(path)?)
+        .lines()
+        .take(7)
+        .flat_map(|line| match line {
+            Ok(ln) => ln.parse_7_values(),
+            _ => vec![String::new(); 7],
+        })
+        .collect::<Vec<_>>();
+    Ok(mvs)
+}
 
-        // choose a mnemonic language if needed
-        if self.has_mnemonic() && self.language.is_none() {
-            self.language = Some(select_language(Language::all())?);
+fn from_inquire() -> Result<Vec<String>> {
+    let mut mvs: Vec<_> = vec![];
+    (1..=7).for_each(|i| {
+        let line = inquire::Text::new(&format!("row ({i})"))
+            .with_initial_value(r#"""  "#.repeat(7).trim_end())
+            .with_help_message("Fill characters in quotes.")
+            .prompt();
+
+        match line {
+            Ok(ln) => mvs.append(&mut ln.parse_7_values()),
+            _ => eprintln!("Error reading input for row {i}"),
         }
+    });
+    Ok(mvs)
+}
 
-        // inquire the encryption password as salt
-        if self.password.is_none() {
-            self.password = Some(inquire_password(true)?);
-        }
+trait LineParser {
+    fn parse_7_values(&self) -> Vec<String>;
+}
 
-        // output the diagram's result
-        ComplexDiagram(mx).display(self)?;
-        Ok(())
+impl LineParser for str {
+    fn parse_7_values(&self) -> Vec<String> {
+        let line = unicode_decode(self.trim());
+        line.strip_prefix('"')
+            .unwrap_or(&self)
+            .strip_suffix('"')
+            .unwrap_or(&self)
+            .split(r#""  ""#)
+            .chain([""; 7])
+            .take(7)
+            .map(|s| s.chars().take(WORD_MAX_LENGTH).collect())
+            .collect::<Vec<_>>()
     }
 }
